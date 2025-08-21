90d465ff6d9fb541a9f51dbe001395204820162b
Loganayagi <varshu958@gmail.com>
2025-08-21 21:54:16 +0530

    Time: 0 ms (100%), Space: 11.4 MB (72.23%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
-        //vector<string> res;
-        unordered_set<string> res;
-        for(int i=0;i<words.size();i++)
-        {
-            for(int j=0;j<words.size();j++)
-            {
-                if(i==j)    continue;
-                if(words[j].find(words[i]) != string::npos)
-                {
-                    res.insert(words[i]);
+        vector<string> ans;
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+                if (i != j && words[j].find(words[i]) != string::npos) {
+                    ans.push_back(words[i]);
+                    break;
                 }
             }
         }
-        //unique(res.begin(),res.end());
-        return vector<string>(res.begin(),res.end());
+        return ans;
     }
 };
