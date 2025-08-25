0640c2b87d25a9e4d3ab3c76c96fb9aa4c7df9d7
Loganayagi <varshu958@gmail.com>
2025-08-25 10:47:35 +0530

    Time: 0 ms (100%), Space: 9 MB (90.98%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.cpp
@@ -1,42 +1,18 @@
 class Solution {
 public:
-    bool vowel(char c)
-    {
-        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' ||c=='O'||c=='U')
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
+    bool vowel(char c) {
+        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+               c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
     }
+
     bool halvesAreAlike(string s) {
-        vector<char> res1;
-        vector<char> res2;
-        int mid=s.size()/2;
-        int count1=0,count2=0;
-        for(int i=0;i<mid;i++)
-        {
-            res1.push_back(s[i]);
+        int mid = s.size() / 2;
+        int count1 = 0, count2 = 0;
+
+        for (int i = 0; i < mid; i++) {
+            if (vowel(s[i])) count1++;
+            if (vowel(s[i + mid])) count2++;
         }
-        for(int i=mid;i<s.size();i++)
-        {
-            res2.push_back(s[i]);
-        }
-        for(int i=0;i<mid;i++)
-        {
-            if(vowel(res1[i]))
-            {
-                count1++;
-            }
-            if(vowel(res2[i]))
-            {
-                count2++;
-            }
-        }
-        if(count1==count2)
-            return true;  
-        return false;
-        
+        return count1 == count2;
     }
 };
