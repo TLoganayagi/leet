f6022212a8d25299c08e6ca0eaa239bb898ac550
Loganayagi <varshu958@gmail.com>
2025-08-25 19:24:06 +0530

    Time: 8 ms (16.86%), Space: 13.4 MB (83.06%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.cpp
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> res;
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             res.push_back(nums[i]);
             res.push_back(nums[i+n]);
         }
         return res;
-        
     }
 };
