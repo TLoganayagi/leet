09cd91adff449c6a96523fdfc4be7030341710f1
Loganayagi T <varshu958@gmail.com>
2025-09-15 09:49:57 +0530

    Time: 13 ms (54%), Space: 23.6 MB (30.8%) - LeetHub

--- 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.cpp
+++ 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
     int countWords(vector<string>& words1, vector<string>& words2) {
-        unordered_map<string ,int> map1;
-        int ans=0;
-        for(string s : words1)
+        unordered_map<string,int> um1;
+        unordered_map<string,int> um2;
+        for(string s:words1)
         {
-            if(map1.find(s)!= map1.end())
+            um1[s]++;
+        }
+        for(string s:words2)
+        {
+            um2[s]++;
+        }
+        int res=0;
+        for(auto &p:um1)
+        {
+            if(p.second==1 && um2[p.first]==1)
             {
-                map1[s]++;
-                continue;
+                res++;
             }
+        }
+        return res;
         
-            map1[s]=0;
-        }
-        for(auto it = map1.begin();it!=map1.end();   )
-        {
-            if(it->second!=0)
-            {
-                it = map1.erase(it);
-            }
-            else
-                it++;
-        }
-        for(string s : words2)
-        {
-            if(map1.find(s)== map1.end())
-            {
-                continue;
-            }
-            map1[s]++;
-        }
-        for(const auto& [key,value] : map1)
-        {
-            if(value == 1)
-            {
-                ans++;
-            }
-        }
-        return ans;
     }
 };
