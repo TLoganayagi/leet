b7c94a50fbecdd448a81ead5f6a8d734278d3c5b
Loganayagi T <varshu958@gmail.com>
2025-11-21 18:56:15 +0530

    Time: 0 ms (100%), Space: 43.8 MB (43.24%) - LeetHub

--- 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.cpp
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
-    bool isAcronym(vector<string>& words, string s) 
-    {
-        string ans="";
-        for(int i=0;i<words.size();i++)
-        {
-            ans+=words[i][0];
-            if(words[i][0]!=s[i])
-                return false;
+    bool isAcronym(vector<string>& words, string s) {
+        string ans = "";
+        for(auto it : words){
+            ans+=it[0];
         }
-        if(ans==s)
-            return true;
-        else
-            return false;   
+        return ans == s;
     }
 };
