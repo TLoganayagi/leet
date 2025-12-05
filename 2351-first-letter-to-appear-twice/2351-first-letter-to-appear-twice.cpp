3127d883fb1900b094d213b22c244f47a0041e4c
Loganayagi T <varshu958@gmail.com>
2025-12-05 23:08:30 +0530

    Time: 0 ms (100%), Space: 8.3 MB (62.75%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_map<char,int> mp;
+        char ch;
+        for(char c:s)
+        {
+            mp[c]++;
+            if (mp[c]==2){
+               ch=c;
+               break; 
+            }
+        }
+        return ch;
+    }
+};
