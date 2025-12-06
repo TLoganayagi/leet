fc16dda0f5b5b77ad472996c54f4088242871a36
Loganayagi T <varshu958@gmail.com>
2025-12-06 12:43:16 +0530

    Time: 2 ms (56.23%), Space: 12 MB (13.26%) - LeetHub

---
+++ 0482-license-key-formatting/0482-license-key-formatting.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void to_upper(string &s)
+    {
+        for(char &c:s)
+        {
+            c=toupper(c);
+        }
+    }
+    string licenseKeyFormatting(string s, int k) {
+        string temp="";
+        for(char c:s)
+        {
+            if(c!='-')
+                temp+=c;
+        }
+        to_upper(temp);
+        int n=temp.size();
+        if(n==0)    return"";
+        int y=n%k;
+        //int x=n/k;
+        string res="";
+        
+        int i=0;
+        if(y!=0)
+        {
+            res+=temp.substr(0,y);
+            res+='-';
+            i=y;
+        }
+
+        while(i<n)
+        {
+            res+=temp.substr(i,k);
+            //res+='-';
+            i+=k;
+            if(i<n) res+='-';
+        }
+        if(res.back() == '-')
+            res.pop_back();
+        return res;
+    }
+};
