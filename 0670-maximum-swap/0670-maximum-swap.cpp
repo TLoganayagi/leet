34fbfbbb23d6c09d089d91be6426ac6a49f4250b
Loganayagi T <varshu958@gmail.com>
2025-12-09 23:55:52 +0530

    Time: 0 ms (100%), Space: 8 MB (29.2%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumSwap(int num) 
+    {
+       auto s=to_string(num);
+       for(int i=0;i<s.size();i++)
+       {
+            auto p=max_element(s.rbegin(),s.rend()-i);
+            if(s[i]!=*p)
+            {
+                swap(s[i],*p);
+                break;
+            }
+       }
+       return stoi(s);
+    }
+};
