8b2172ce77a0f04d8487f7e26607b9ee055fbe7f
Loganayagi T <varshu958@gmail.com>
2025-12-10 00:26:14 +0530

    Time: 8 ms (66.14%), Space: 175.3 MB (96.81%) - LeetHub

--- 3584-maximum-product-of-first-and-last-elements-of-a-subsequence/3584-maximum-product-of-first-and-last-elements-of-a-subsequence.cpp
+++ 3584-maximum-product-of-first-and-last-elements-of-a-subsequence/3584-maximum-product-of-first-and-last-elements-of-a-subsequence.cpp
@@ -1,16 +1,15 @@
+// Idea: greedy, prefix max or prefix min, scan, sliding window.
+// if m == 1, then just find the element with max abs.
+// let pmax be the max in A[0...i-m]
 class Solution {
 public:
-    long long maximumProduct(vector<int>& nums, int m) 
-    {
-        long long mn = INT_MAX, mx = INT_MIN, res = LLONG_MIN;
-        for (int i = 0; i < nums.size(); ++i)
-        {
-            if (i + 1 >= m) 
-            {
-                mn = min(mn, 1LL * nums[i + 1 - m]);
-                mx = max(mx, 1LL * nums[i + 1 - m]);
-                res = max({res, mn * nums[i], mx * nums[i]});
-            }
+    long long maximumProduct(vector<int>& A, int m) {
+        long long res = LONG_LONG_MIN;
+        for (int i = m - 1, pmax = A[0], pmin = A[0]; i < A.size(); ++i) {
+            pmax = max(pmax, A[i - m + 1]);
+            pmin = min(pmin, A[i - m + 1]);
+            res = max(res, (long long)pmax * A[i]);
+            res = max(res, (long long)pmin * A[i]);
         }
         return res;
     }
