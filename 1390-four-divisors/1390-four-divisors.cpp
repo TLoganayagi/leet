ae38cdca400a93f1f90b5ba73520973d1abe6a06
Loganayagi T <varshu958@gmail.com>
2026-01-05 12:43:13 +0530

    Time: 12 ms (70.24%), Space: 16.2 MB (48.05%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     int sumFourDivisors(vector<int>& nums) {
-        int res = 0;
-        for (int n : nums) {
-            int val = sumOne(n);
-            if (val != -1) res += val;
-        }
-        return res;
-    }
-
-    int sumOne(int n) {
-        int p = round(cbrt(n));
-        if ((long long)p * p * p == n && isPrime(p)) {
-            return 1 + p + p*p + p*p*p;
-        }
-
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                int a = i, b = n / i;
-                if (a != b && isPrime(a) && isPrime(b)) {
-                    return 1 + a + b + n;
+        int tot_sum=0;
+        for(int x: nums)
+        {
+            int count=0;
+            int sum=0;
+            for(int i=1;i*i<=x;i++)
+            {
+                if(x%i==0)
+                {
+                    int j=x/i;
+                    count++;
+                    sum+=i;
+                    if(i!=j)
+                    {
+                        count++;
+                        sum+=j;
+                    }
+                    if(count>4) break;
                 }
-                return -1;
+            }
+            if(count==4)
+            {
+                tot_sum+=sum;
             }
         }
-        return -1;
-    }
-
-    bool isPrime(int x) {
-        if (x < 2) return false;
-        for (int i = 2; i * i <= x; i++) {
-            if (x % i == 0) return false;
-        }
-        return true;
+        return tot_sum;
     }
 };
