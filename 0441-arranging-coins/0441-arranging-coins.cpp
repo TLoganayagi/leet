aded43ddf90a9f164625f14586e329d45866a3f2
Loganayagi T <varshu958@gmail.com>
2026-01-07 11:44:28 +0530

    Time: 0 ms (100%), Space: 8.8 MB (33.7%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        return floor(-0.5+sqrt((double)2*n+0.25));
+        //return floor(-0.5+sqrt((double)2*n+0.25));
+        return (int)floor((sqrt(8l*n + 1) + 1)/2) - 1;
     }
 };
